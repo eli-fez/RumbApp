c0b7188f052d6bad97979eb50480b1e8fd3d7f80
Elias <142056401+eli-fez@users.noreply.github.com>
2024-02-16 10:05:13 -0800

    Update UserRoleModel.cs

--- .NET/UserModel.cs
+++ .NET/UserModel.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Rumb.Models.Domain.Users
+namespace Sabio.Models.Domain.Users
 {
-    public class User : BaseUser
+    public class UsersRoleStatus : BaseUser
     {
         public string Email { get; set; }
-        public bool isConfirmed { get; set; }  
-        public string Status { get; set; }
-        public string Role { get; set; }
+        public bool isConfirmed { get; set; }
+        public LookUp Status { get; set; }
+        public LookUp Role { get; set; }
         public DateTime DateCreated { get; set; }
         public DateTime DateModified { get; set; }
 
